74d2c28aeb3ca270bd6dd20b456f55b62d3a3cff
kishorecapgemini <88018173+kishorecapgemini@users.noreply.github.com>
2021-08-09 14:28:12 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,17 +1,29 @@
 import { Button, Grid, Paper, TextField, Typography, Link, Box } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
 import React from 'react';
+import { grey } from '@material-ui/core/colors';
+
+
+
+
 
 const Login = () => {
+    let history = useHistory();
 
     const paperStyle = { padding: '100px 100px 100px 100px', height: '80vh', width: 380, margin: "100px auto" }
 
     const btnstyle = { margin: '8px 0', backgroundColor: '#0878ea' }
-    const emailstyle = { margin: '10px 0' }
-    const passstyle = { margin: '10px 0' }
+    const emailstyle = { margin: '10px 0', color: grey }
+    const passstyle = { margin: '10px 0', color: grey }
     const bt = { padding: '60px 0px 0px 0px' }
     const forgetstyle = { margin: '5px 0' }
     const linkstyle = { color: '#0878ea' }
     const linkkstyle = { color: '#0878ea' }
+    const fontcol = { color: '#696161', fontWeight: 600 }
+
+
+
+
 
 
     return (
@@ -22,21 +34,21 @@ const Login = () => {
                     <Grid align='center'>
 
 
-                        <h3><img src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-abstract-s-letter-circle-vector-logo-design-alphabet-circle-logo-vector-png-image_3611616.jpg" alt="" style={{ width: 35, height: 36, borderRadius: 100 / 3 }} align='center' />     Blog   </h3>
+                        <h2 style={fontcol}><img src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-abstract-s-letter-circle-vector-logo-design-alphabet-circle-logo-vector-png-image_3611616.jpg" alt="" style={{ width: 35, height: 36, borderRadius: 100 / 3 }} align='center' /> Blog </h2>
                     </Grid>
-                    <TextField id="outlined-basic" label='Email address' variant="outlined" placeholder='Enter email address' fullWidth required style={emailstyle} />
-                    <TextField id="outlined-basic" label='Password' variant="outlined" placeholder='Enter password' type='password' fullWidth required style={passstyle} />
+                    <TextField id="outlined-basic" label='Email address' variant="outlined" placeholder='Enter email address' fullWidth style={emailstyle} />
+                    <TextField id="outlined-basic" label='Password' variant="outlined" placeholder='Enter password' type='password' fullWidth style={passstyle} />
 
                     <Typography align='right' style={forgetstyle}>
                         <Link href="#" style={linkkstyle}>
-                            Forgot you password?
+                            Forgot your password?
                         </Link>
                     </Typography>
 
                     <Button type='submit' color='primary' fullWidth variant="contained" style={btnstyle}>Sign In</Button>
 
                     <Typography style={bt}> Don't  have an account?
-                        <Link href="#" style={linkstyle}>
+                        <Link onClick={() => { history.push('/signup') }} href="#" style={linkstyle}>
                             Sign up here
                         </Link>
                     </Typography>
