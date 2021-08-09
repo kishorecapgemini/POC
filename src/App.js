977f0c4c29f5c7495a0e2ff0394a30c54cb6d1ef
kishorecapgemini <kishorecapgemini777@gmail.com>
2021-08-09 14:25:08 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './components/Login';
+import Signup from './components/signup';
 
-function App(){
+function App() {
   return (
     <div className="App">
-      
-      <Login></Login>
-      
+      <Router>
+        <Switch>
+
+          < Route exact path="/login" component={Login} />
+          < Route exact path="/signup" component={Signup} />
+        </Switch>
+      </Router>
+
+
+
     </div>
   );
 }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,61 +1,65 @@
 import { Button, Grid, Paper, TextField, Typography, Link, Box } from '@material-ui/core';
-import React from 'react'; 
+import { useHistory } from 'react-router-dom';
+import React from 'react';
+import { grey } from '@material-ui/core/colors';
 
 
 
 
 
-const Login=()=>{
+const Login = () => {
+    let history = useHistory();
 
-    const paperStyle={padding : '100px 100px 100px 100px',height:'80vh',width:380,margin:"100px auto"}
-    
-    const btnstyle={margin:'8px 0',backgroundColor:'#0878ea'}
-    const emailstyle={margin:'10px 0'}
-    const passstyle ={margin:'10px 0'}
-    const bt ={padding: '60px 0px 0px 0px'}
-    const forgetstyle={margin:'5px 0'}
-    const linkstyle={color:'#0878ea'}
-    const linkkstyle={color:'#0878ea'}
-    
-    
-    
-    //const tr={padding:'15px 0px 20px 130px'}
-    
+    const paperStyle = { padding: '100px 100px 100px 100px', height: '80vh', width: 380, margin: "100px auto" }
 
-    return(
-        
+    const btnstyle = { margin: '8px 0', backgroundColor: '#0878ea' }
+    const emailstyle = { margin: '10px 0', color: grey }
+    const passstyle = { margin: '10px 0', color: grey }
+    const bt = { padding: '60px 0px 0px 0px' }
+    const forgetstyle = { margin: '5px 0' }
+    const linkstyle = { color: '#0878ea' }
+    const linkkstyle = { color: '#0878ea' }
+    const fontcol = { color: '#696161', fontWeight: 600 }
+
+
+
+
+
+
+    return (
+
         <Grid>
             <Paper elevation={10} style={paperStyle}>
-            <Box border={1} borderColor="gray" padding='50px 20px 20px 20px' width='200' height='40vh' margin='30px  auto ' borderRadius='15px'>
-                <Grid align='center'>
-                    
-                    
-                    <h3><img src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-abstract-s-letter-circle-vector-logo-design-alphabet-circle-logo-vector-png-image_3611616.jpg" alt= "" style={{width:35, height:36, borderRadius:100/3}} align='center' />     Blog   </h3>
-                </Grid>
-                <TextField id="outlined-basic" label='Email address' variant="outlined" placeholder='Enter email address' fullWidth required style={emailstyle}/>
-                <TextField id="outlined-basic" label='Password' variant="outlined" placeholder='Enter password' type='password' fullWidth required style={passstyle}/>
-                
-                <Typography align='right'style={forgetstyle}>
-                    <Link href="#" style={linkkstyle}>
-                    Forgot you password?
-                    </Link>
-                </Typography>
-                
-                <Button type='submit' color = 'primary' fullWidth variant="contained" style={btnstyle}>Sign In</Button>
-                
-                <Typography style={bt}> Don't  have an account?
-                    <Link href="#" style={linkstyle}>
-                      Sign up here
-                    </Link>
-                </Typography>
-                
-            </Box>
+                <Box border={1} borderColor="gray" padding='50px 20px 20px 20px' width='200' height='40vh' margin='30px  auto ' borderRadius='15px'>
+                    <Grid align='center'>
+
+
+                        <h2 style={fontcol}><img src="https://png.pngtree.com/png-clipart/20200709/original/pngtree-abstract-s-letter-circle-vector-logo-design-alphabet-circle-logo-vector-png-image_3611616.jpg" alt="" style={{ width: 35, height: 36, borderRadius: 100 / 3 }} align='center' /> Blog </h2>
+                    </Grid>
+                    <TextField id="outlined-basic" label='Email address' variant="outlined" placeholder='Enter email address' fullWidth style={emailstyle} />
+                    <TextField id="outlined-basic" label='Password' variant="outlined" placeholder='Enter password' type='password' fullWidth style={passstyle} />
+
+                    <Typography align='right' style={forgetstyle}>
+                        <Link href="#" style={linkkstyle}>
+                            Forgot your password?
+                        </Link>
+                    </Typography>
+
+                    <Button type='submit' color='primary' fullWidth variant="contained" style={btnstyle}>Sign In</Button>
+
+                    <Typography style={bt}> Don't  have an account?
+                        <Link onClick={() => { history.push('/signup') }} href="#" style={linkstyle}>
+                            Sign up here
+                        </Link>
+                    </Typography>
+
+                </Box>
             </Paper>
-            
+
         </Grid>
-        
+
     );
-    
+
 }
 
 export default Login;
