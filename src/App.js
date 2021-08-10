c0cbe4c4dfce5a80894d452672e8a56e5d285667
kishorecapgemini <88018173+kishorecapgemini@users.noreply.github.com>
2021-08-10 22:32:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 import React, { lazy, Suspense } from 'react';
-=======
-import React ,{ lazy,Suspense } from 'react';
->>>>>>> 607863304927b719a9d730736d9e2fcf0d9b9e3e
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 const Login = lazy(() => import('./components/Login'))
@@ -13,7 +9,6 @@ function App() {
   return (
     <div className="App">
       <Suspense fallback={renderLoader()}>
-<<<<<<< HEAD
         <Router>
           <Switch>
 
@@ -21,15 +16,6 @@ function App() {
             < Route exact path="/signup" component={Signup} />
           </Switch>
         </Router>
-=======
-      <Router>
-        <Switch>
-
-          < Route exact path="/login" component={Login} />
-          < Route exact path="/signup" component={Signup} />
-        </Switch>
-      </Router>
->>>>>>> 607863304927b719a9d730736d9e2fcf0d9b9e3e
       </Suspense>
 
 
